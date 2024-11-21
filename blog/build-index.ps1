6d0500f8a9869abe7f8889b79c91b3e25f36f2b9
Ákos Nikházy <604311+akosnikhazy@users.noreply.github.com>
2024-11-21 08:16:31 +0100

    fixed rss generation and added sitemap generator

--- blog/build-index.ps1
+++ blog/build-index.ps1
@@ -1,8 +1,12 @@
 $HTMLtemplatePath = "index-template.html"
 $RSStemplatePath = "rss-template.xml"
+$SitemaptemplatePath = "sitemap-template.xml"
+
 $contentFolderPath = "posts"
+
 $HTMLOutput = "index.html"
 $RSSOutput = "rss.xml"
+$SitemapOutput = "sitemap.xml"
 
 
 $CultureInfo = [System.Globalization.CultureInfo]::CreateSpecificCulture("en-US")
@@ -11,12 +15,19 @@ $CultureInfo = [System.Globalization.CultureInfo]::CreateSpecificCulture("en-US"
 
 $HTMLtemplateContent = Get-Content -Path $HTMLtemplatePath -Raw
 $RSStemplateContent = Get-Content -Path $RSStemplatePath -Raw
+$SitemaptemplateContent = Get-Content -Path $SitemaptemplatePath -Raw
 
 $htmlFiles = Get-ChildItem -Path $contentFolderPath -Filter "*.html" -Recurse
 
 
 $HTMLListContent = ""
 $RSSListContent = ""
+$SitemapListContent = ""
+
+
+$formattedCurrentDate = $currentDate.ToString("ddd, dd MMM yyyy HH:mm:ss")
+
+
 
 foreach ($htmlFile in $htmlFiles) {
    
@@ -34,25 +45,31 @@ foreach ($htmlFile in $htmlFiles) {
         $pText = $matches[1]
     }
 
+
+
     $relativePath = "$containingFolder/$htmlFile"
 
     $HTMLListContent += '<div><a href="posts/' + $relativePath + '"><div class="posttime">' + $containingFolder + '</div>' + $h1Text + '<br>'+ $pText + '</a></div>'
-    $RSSListContent += '<item><title>' + $h1Text + '</title></item><link>https://yzahkin.games/blog/posts/' + $relativePath + '</link><description>' + $pText + '</description>'
-
     
+    $sha256 = [System.Security.Cryptography.SHA256]::Create()
+    $guid =  [BitConverter]::ToString($sha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($h1Text + $formattedCurrentDate))) -replace '-'
+
+    $RSSListContent += '<item><guid isPermaLink="false">' + $guid + '</guid><title>' + $h1Text + '</title><link>https://yzahkin.games/blog/posts/' + $relativePath + '</link><description>' + $pText + '</description></item>'
+
+    $SitemapListContent += "<url><loc>https://yzahkin.games/blog/posts/$containingFolder</loc><lastmod>$containingFolder</lastmod></url>"
 
 }
 
 
 $HTMLtemplateContent = $HTMLtemplateContent -replace "\{\{list\}\}", $HTMLListContent
-
 $RSStemplateContent = $RSStemplateContent -replace "\{\{items\}\}", $RSSListContent
+$SitemaptemplateContent = $SitemaptemplateContent -replace "\{\{urls\}\}", $SitemapListContent
 
-$currentDate = Get-Date
-$formattedCurrentDate = $currentDate.ToString("ddd, dd MMM yyyy HH:mm:ss zzz")
-$RSStemplateContent = $RSStemplateContent -replace "\{\{date\}\}", $formattedCurrentDate
+
 
 
 Set-Content -Path $HTMLOutput -Value $HTMLtemplateContent
 Set-Content -Path $RSSOutput -Value $RSStemplateContent
+Set-Content -Path $SitemapOutput -Value $SitemaptemplateContent
 Write-Host "Template updated and saved to $modifiedFilePath"
+
