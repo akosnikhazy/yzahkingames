7196bf1bfd59369b95b72af5b3279f5a86608b34
Ákos Nikházy <604311+akosnikhazy@users.noreply.github.com>
2024-11-21 10:24:50 +0100

    finished

--- blog/build-index.ps1
+++ blog/build-index.ps1
@@ -1,4 +1,5 @@
 $HTMLtemplatePath = "index-template.html"
+$PosttemplatePath = "post-template.html"
 $RSStemplatePath = "rss-template.xml"
 $SitemaptemplatePath = "sitemap-template.xml"
 
@@ -9,25 +10,24 @@ $RSSOutput = "rss.xml"
 $SitemapOutput = "sitemap.xml"
 
 
-$CultureInfo = [System.Globalization.CultureInfo]::CreateSpecificCulture("en-US")
-[System.Threading.Thread]::CurrentThread.CurrentCulture = $CultureInfo
-[System.Threading.Thread]::CurrentThread.CurrentUICulture = $CultureInfo
-
 $HTMLtemplateContent = Get-Content -Path $HTMLtemplatePath -Raw
 $RSStemplateContent = Get-Content -Path $RSStemplatePath -Raw
 $SitemaptemplateContent = Get-Content -Path $SitemaptemplatePath -Raw
+$postContent = Get-Content -Path $PosttemplatePath -Raw
 
-$htmlFiles = Get-ChildItem -Path $contentFolderPath -Filter "*.html" -Recurse
+$directories = Get-ChildItem -Path $contentFolderPath -Directory | Sort-Object Name -Descending
 
+$htmlFiles = @()
+
+foreach ($dir in $directories) {
+    $htmlFiles += Get-ChildItem -Path $dir.FullName -Filter "*.html"
+}
 
 $HTMLListContent = ""
 $RSSListContent = ""
 $SitemapListContent = ""
 
-
-$formattedCurrentDate = $currentDate.ToString("ddd, dd MMM yyyy HH:mm:ss")
-
-
+$postCount = 0
 
 foreach ($htmlFile in $htmlFiles) {
    
@@ -49,27 +49,30 @@ foreach ($htmlFile in $htmlFiles) {
 
     $relativePath = "$containingFolder/$htmlFile"
 
-    $HTMLListContent += '<div><a href="posts/' + $relativePath + '"><div class="posttime">' + $containingFolder + '</div>' + $h1Text + '<br>'+ $pText + '</a></div>'
-    
+    $postText = $postContent;
+
+    $postText = $postText -replace "\{\{path\}\}",$relativePath
+    $postText = $postText -replace "\{\{title\}\}", $h1Text
+    $postText = $postText -replace "\{\{date\}\}", $containingFolder
+    $postText = $postText -replace "\{\{text\}\}", $pText
+
+    $HTMLListContent +=   $postText
+
     $sha256 = [System.Security.Cryptography.SHA256]::Create()
-    $guid =  [BitConverter]::ToString($sha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($h1Text + $formattedCurrentDate))) -replace '-'
+    $guid =  [BitConverter]::ToString($sha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($h1Text + $containingFolder + $relativePath))) -replace '-'
 
     $RSSListContent += '<item><guid isPermaLink="false">' + $guid + '</guid><title>' + $h1Text + '</title><link>https://yzahkin.games/blog/posts/' + $relativePath + '</link><description>' + $pText + '</description></item>'
 
     $SitemapListContent += "<url><loc>https://yzahkin.games/blog/posts/$containingFolder</loc><lastmod>$containingFolder</lastmod></url>"
 
+    $postCount++
 }
-
 
 $HTMLtemplateContent = $HTMLtemplateContent -replace "\{\{list\}\}", $HTMLListContent
 $RSStemplateContent = $RSStemplateContent -replace "\{\{items\}\}", $RSSListContent
 $SitemaptemplateContent = $SitemaptemplateContent -replace "\{\{urls\}\}", $SitemapListContent
 
-
-
-
 Set-Content -Path $HTMLOutput -Value $HTMLtemplateContent
 Set-Content -Path $RSSOutput -Value $RSStemplateContent
 Set-Content -Path $SitemapOutput -Value $SitemaptemplateContent
-Write-Host "Template updated and saved to $modifiedFilePath"
-
+Write-Host "$HTMLOutput, $RSSOutput and $SitemapOutput built with $postCount post(s)"
